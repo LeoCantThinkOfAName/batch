46ce702184ad73acb845cbc244115fdbf88803e7
leocantthinkofaname <i7851@hotmail.com>
2017-08-03 09:19:03 +0800

    ver. 1

---
+++ Android Emulator Picker.bat
@@ -0,0 +1,17 @@
+:PICKER
+@ECHO off
+::VERSION?
+ECHO Please Select Ther Version of Android You Wanna use
+ECHO 1. v5 Lollipop
+ECHO 2. v4 KitKat
+SET /P VERSION=Pick One:
+
+CD C:/users/Leo/AppData/Local/Android/sdk/tools
+IF "%VERSION%"=="1" (
+    START emulator -avd Nexus_5X_API_21
+) ELSE IF "%VERSION%"=="2" (
+    START emulator -avd Nexus_5X_API_16
+) ELSE (
+    CLS
+    GOTO PICKER
+)
